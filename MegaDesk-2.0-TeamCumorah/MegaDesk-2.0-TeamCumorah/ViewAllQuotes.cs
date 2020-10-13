cd0b2fd81b52c2063205a9f5462305fe710f3b35
Hannah <rog19008@byui.edu>
2020-10-13 12:51:34 -0600

    added DataGridView to ViewAllQuotes

--- MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/ViewAllQuotes.cs
+++ MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/ViewAllQuotes.cs
@@ -12,9 +12,38 @@ namespace MegaDesk_2._0_TeamCumorah
 {
     public partial class ViewAllQuotes : Form
     {
+        private DataGridView dgv = new DataGridView();
         public ViewAllQuotes()
         {
             InitializeComponent();
+            SetupDataGridView();
+            PopulateDataGridView();
+        }
+
+        private void SetupDataGridView()
+        {
+            this.Controls.Add(dgv);
+
+            dgv.ColumnCount = 5;
+
+            dgv.Name = "quotesGridView";
+            dgv.Location = new Point(8, 8);
+            dgv.Size = new Size(550, 400);
+
+            dgv.Columns[0].Name = "Customer Name";
+            dgv.Columns[1].Name = "Date";
+            dgv.Columns[2].Name = "Material";
+            dgv.Columns[3].Name = "Rush Days";
+            dgv.Columns[4].Name = "Price";
+        }
+
+        private void PopulateDataGridView()
+        {
+            foreach (Quote q in QuotesList.quotes)
+            {
+                string[] row = { q._customerName, q.date, q.deskMaterial, q.rushDays.ToString(), "$" + q.totalQuote.ToString()};
+                dgv.Rows.Add(row);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
--- MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/ViewAllQuotes.designer.cs
+++ MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/ViewAllQuotes.designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(336, 189);
+            this.button1.Location = new System.Drawing.Point(660, 366);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 72);
             this.button1.TabIndex = 1;
