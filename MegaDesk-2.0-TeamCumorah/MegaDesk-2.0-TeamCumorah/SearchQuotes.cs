c5ea6f67178fbc1066077706f4a0d798ae40dd97
onwuchekwa <donsonde@gmail.com>
2020-10-13 18:36:20 +0000

    Search form added and tested

--- MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/SearchQuotes.cs
+++ MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/SearchQuotes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +17,90 @@ namespace MegaDesk_2._0_TeamCumorah
         {
             InitializeComponent();
         }
-        
+
+        /// <summary>
+        /// This binds data to the material combobox from an enumerator
+        /// </summary>
+        public void BindMaterial()
+        {
+            List<Desk.DesktopMaterial> desktopMaterials = Enum.GetValues(typeof(Desk.DesktopMaterial)).Cast<Desk.DesktopMaterial>().ToList();
+            cbMaterials.DataSource = desktopMaterials;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             // Close the form
             MainMenu mainMenu = (MainMenu)Tag;
             mainMenu.Show();
             Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lvQuoteList.Clear(); 
+            lvQuoteList.Items.Clear();
+            lvQuoteList.View = View.Details;
+
+            try
+            { 
+
+            string deskMaterials = cbMaterials.SelectedItem.ToString();
+
+            lvQuoteList.Columns.Add("#", 30, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Client Name", 200, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Date", 180, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Width", 70, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Depth", 70, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Drawers", 70, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Material", 100, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Rush Days", 70, HorizontalAlignment.Left);
+            lvQuoteList.Columns.Add("Total Amount", 150, HorizontalAlignment.Left);
+
+            string dir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            dir = dir.Remove(dir.IndexOf("\\bin\\Debug"));
+            //reads in the file
+            using (StreamReader r = new StreamReader(dir + @"\quotes.json"))
+            {
+                int rowCount = 0;
+
+                    while (!r.EndOfStream)
+                    {
+                        //read
+                        string json = r.ReadToEnd();
+                        //parse json into a List of Quotes within QuotesList
+                        List<Quote> quotes = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Quote>>(json);
+
+                        foreach (Quote quote in quotes)
+                        {
+                            if (quote.deskMaterial == deskMaterials)
+                            {
+                                rowCount++;
+                                lvQuoteList.Items.Add(new ListViewItem(new[]
+                                {
+                                    rowCount.ToString(),
+                                    quote._customerName,
+                                    quote.date,
+                                    quote.deskWidth.ToString(),
+                                    quote.deskDepth.ToString(),
+                                    quote.deskDrawer.ToString(),
+                                    quote.deskMaterial,
+                                    quote.rushDays.ToString(),
+                                    quote.totalQuote.ToString("C2")
+                                })); 
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error populating the report: " + "\n\n" + ex.Message);
+            }
+        }
+
+        private void SearchQuotes_Load(object sender, EventArgs e)
+        {
+            BindMaterial();
         }
     }
 }
--- MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/SearchQuotes.designer.cs
+++ MegaDesk-2.0-TeamCumorah/MegaDesk-2.0-TeamCumorah/SearchQuotes.designer.cs
@@ -28,35 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbMaterials = new System.Windows.Forms.ComboBox();
+            this.lvQuoteList = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
-            // button1
+            // btnClose
             // 
-            this.button1.Location = new System.Drawing.Point(286, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 72);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Close form";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(605, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(128, 30);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close form";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(459, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(128, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbMaterials
+            // 
+            this.cbMaterials.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMaterials.FormattingEnabled = true;
+            this.cbMaterials.Location = new System.Drawing.Point(214, 12);
+            this.cbMaterials.Name = "cbMaterials";
+            this.cbMaterials.Size = new System.Drawing.Size(229, 27);
+            this.cbMaterials.TabIndex = 1;
+            // 
+            // lvQuoteList
+            // 
+            this.lvQuoteList.HideSelection = false;
+            this.lvQuoteList.Location = new System.Drawing.Point(12, 57);
+            this.lvQuoteList.Name = "lvQuoteList";
+            this.lvQuoteList.Size = new System.Drawing.Size(920, 381);
+            this.lvQuoteList.TabIndex = 4;
+            this.lvQuoteList.UseCompatibleStateImageBehavior = false;
             // 
             // SearchQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(944, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lvQuoteList);
+            this.Controls.Add(this.cbMaterials);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClose);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
+            this.Load += new System.EventHandler(this.SearchQuotes_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cbMaterials;
+        private System.Windows.Forms.ListView lvQuoteList;
     }
 }
