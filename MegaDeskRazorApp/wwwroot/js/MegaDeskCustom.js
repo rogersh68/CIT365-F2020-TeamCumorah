edd2345976dcf24c7e44ded082c346556376eb71
BassettJake <35384947+BassettJake@users.noreply.github.com>
2020-11-07 16:34:09 -0700

    background image

--- MegaDeskRazorApp/wwwroot/js/MegaDeskCustom.js
+++ MegaDeskRazorApp/wwwroot/js/MegaDeskCustom.js
@@ -30,3 +30,60 @@ eles[1].addEventListener('focus', function () {
 eles[1].addEventListener('blur', function () {
     ele1.setAttribute("style", "rgba(255,255,255,.5) !important");
 });
+
+var ele = document.getElementById("woodType");
+
+var e = ele;
+if (!(ele === null)) {
+    ele.addEventListener('change', function () {
+
+        var child = e.children;
+        for (var i = 0; i < child.length; i++) {
+            if (child[i].selected == true && child[i].value == 50) {
+                e.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/PineWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 100) {
+                e.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/LaminateWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 125) {
+                e.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/VeneerWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 200) {
+                e.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/OakWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 300) {
+                e.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/RoseWood.PNG); background-size: 100% auto;");
+            }
+        }
+
+    });
+
+}
+
+var eleEdit = document.getElementById("Quote_desktopMaterial");
+
+var eEdit = eleEdit;
+if (!(eleEdit === null)) {
+    eleEdit.addEventListener('change', function () {
+
+        var child = eEdit.children;
+        for (var i = 0; i < child.length; i++) {
+            if (child[i].selected == true && child[i].value == 50) {
+                eEdit.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/PineWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 100) {
+                eEdit.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/LaminateWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 125) {
+                eEdit.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/VeneerWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 200) {
+                eEdit.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/OakWood.PNG); background-size: 100% auto;");
+            }
+            else if (child[i].selected == true && child[i].value == 300) {
+                eEdit.setAttribute("style", "background-image: linear-gradient(rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url(../images/RoseWood.PNG); background-size: 100% auto;");
+            }
+        }
+
+    });
+}
